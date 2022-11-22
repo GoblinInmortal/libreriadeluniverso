8421f5a865fccd98be20931d4b9641656cce4a1c
GoblinInmortal <anyelo.carcausto.alarcon@gmail.com>
2022-11-22 09:55:41 -0500

    V08 Actualizar y eliminar un documento elegido

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import { collection, getDocs, query, doc, deleteDoc, where, } from "firebase/firestore";
-import { useState } from "react";
-import firebase, {db} from './componente/firebase';
+import { collection, onSnapshot, where, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import AppForm from "./componente/AppForm";
+import {db} from "./componente/firebase";
 
-function App() {
-  
-  //////////////////// Read  fnRead //////////////////////////
-  const [idActual, setIdActual] = useState(""); 
-  const fnRead = (e) => {
-    console.log("Lectura base de datos"); 
+function App() {  
+  ///////////////////////////////////////////////////////////////////////
+  ////////// READ - fnRead - LECTURA A BD ///////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  const [docsBD, setDocsBD] = useState([]);
+  //console.log(docsBD);
+
+  const fnRead = () => {
+    const xColeccionConQuery = query(collection(db, "persona"));
+    //const xColeccionConQuery = query(collection(db, "persona"), where("nombre", "!=", ""));
+    const unsubcribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+      const xDoc = [];
+      xDatosBD.forEach( (doc) => {
+        xDoc.push({id: doc.id, ...doc.data()});
+      });
+
+      setDocsBD(xDoc);
+    });
+  }
+
+  fnRead();
+/*
+  useEffect( () => {
+    
+  }, [] );
+  */
+  ///////////////////////////////////////////////////////////////////////
+  ////////// DELETE - fnDelete - Eliminar registros /////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  const [idActual, setIdActual] = useState("");
+
+  const fnDelete = (xId) => {
+    console.log("Se elimino con exito..."+xId);
   };
-  //////////////////// Delete  fnDelete //////////////////////
-  const fnDelete = (e) => {
-     console.log("Se elimino con exito...");
-  };
-  
+
   return (
-    <div style={{background:"orange", padding:"10px", width:"350px"}}>
-      <h1>App.js</h1>
-      <AppForm {...{idActual, setIdActual, fnRead}}   />
+    <div style={{width:"350px", background:"black", padding:"10px"}}>
+      <h1>Mi sitio (App.js)</h1>
+      <h3>READ / DELETE</h3>
+      <AppForm {...{idActual, setIdActual, fnRead}} />
+      {
+        docsBD.map((row) =>
+        <p key={row.id}>
+          {row.nombre} ...  
+          <span onClick={() => fnDelete(row.id)}> x </span>
+          ...
+          <span onClick={() => setIdActual(row.id)}> A </span>
+        </p>
+        )
+      }      
     </div>
   );
 }
--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -1,62 +1,74 @@
-import {collection, doc, getDocs, addDoc, updateDoc } from "firebase/firestore";
-
-import React, { useState } from 'react';
-import firebase, {db} from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
+import React, {useState} from 'react'
+import {db} from "./firebase";
 
 const AppForm = (props) => {
-    //////////////////// Create  fnCreate //////////////////////
-    const camposRegistro ={nombre:"", edad:"", genero:""}; //Estructura del objeto (Tabla)
-    const [objeto, setObjeto] = useState(camposRegistro); //Tabla o objeto
+    ///////////////////////////////////////////////////////////////////////
+    ////////// CREAR - fnCrear - Guardar //////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    const camposRegistro = {nombre:"", edad:"", genero:""};
+    const [objeto, setObjeto] = useState(camposRegistro);
 
-    const handleStatusChange = (e) => {
+    const handleStatusChange = (e) => {      //Manejar cambios en form
         const {name, value} = e.target;
         setObjeto({...objeto, [name]:value });
         console.log(objeto);
     };
 
-    const handleSubmit = (e) => {   
+    const handleSubmit = (e) => {
         e.preventDefault();
-        //////// Registrar //////////
-        if(props.idActual === ""){
-            console.log(props.idActual);
-            if(validarForm()){
-                addDoc(collection(db, 'persona'), objeto); //CREAR
-                console.log('Se guardo...');
-                props.fnRead();
-            }else{
-                console.log('No se guardó...');
-            }
-        }else{
-        }
-        setObjeto(camposRegistro); // Limpiar objeto
-        console.log("Maneja envio");
-    }
 
-    const validarForm  = () => {
-        if(objeto.nombre ==="" || /^\s+$/.test(objeto.nombre)){
-            alert("Escriba nombres...");
+        try {
+            if(props.idActual === ""){
+                if(validarForm()){
+                    addDoc(collection(db, 'persona'), objeto);
+                    console.log("Se guardo registro en BD...");
+                }else{
+                    console.log("NO se guardo...");
+                }
+            }else{
+                console.log("ACTUALIZAR REGISTRO..." +props.idActual);
+            }
+            
+        } catch (error) {
+            console.error();
+        } 
+    };
+    
+    const validarForm = () => {
+         if(objeto.nombre === ""){
+            alert("Escriba nombre...");
             return false;
-        }
-        return true;
+         }
+         return true;
     };
 
-    //////////////////// Update  fnUpdate //////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ////////// UPDATE - fnUpdate - Actualizar /////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+
     return (
-        <div style={{background:"yellow", padding:"10px", margin:"10px"}}>
-            <h1>AppForm.js</h1>
-            <form onSubmit ={handleSubmit}>   
-                <input type ="text" name = "nombre" placeholder = "Nombre"
-                onChange = {handleStatusChange} value = {objeto.nombre}/> <br/>
-                <input type ="text" name = "edad" placeholder = "Edad"
-                onChange = {handleStatusChange} value = {objeto.edad}/> <br/>
-                                <input type ="text" name = "genero" placeholder = "Genero"
-                onChange = {handleStatusChange} value = {objeto.genero}/> <br/>
+        <div style={{background:"Gray", padding:"10px", margin:"10px"}}>
+            <h3>CREAR / UPDATE</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='nombre' placeholder='Nombres...' 
+                    onChange={handleStatusChange} value={objeto.nombre}
+                />
+
+                <input type="text" name='edad' placeholder='Edad...' 
+                    onChange={handleStatusChange} value={objeto.edad}
+                />
+
+                <input type="text" name='genero' placeholder='Genero...' 
+                    onChange={handleStatusChange} value={objeto.genero}
+                />
                 <button>
-                    {props.idActual === ""? "Guarda" : "Actualizar"}
+                    {props.idActual === "" ? "Guardar" : "Actualizar" }
                 </button>
             </form>
+            
         </div>
     )
 }
 
-export default AppForm;
+export default AppForm
